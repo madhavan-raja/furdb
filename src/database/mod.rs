14149c05a8489cab43586f55a786269dcb8bb119
madhavan-raja <madhavanraja99@gmail.com>
2022-03-16 22:47:33 +0530

    DBs without info files can now be accessed

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,26 +1,29 @@
 use crate::FurDBInfo;
-use std::path::PathBuf;
+use std::{error::Error, path::PathBuf};
 
 #[derive(Debug)]
 pub struct FurDB {
     dir: PathBuf,
+    db_info: FurDBInfo,
 }
 
 mod operations;
 mod utils;
 
 impl FurDB {
-    pub fn new(dir: PathBuf, db_info: Option<FurDBInfo>) -> std::io::Result<FurDB> {
-        Self::ensure_db_files(&dir, db_info)?;
-        Ok(FurDB { dir })
+    pub fn new(dir: PathBuf, db_info: Option<FurDBInfo>) -> Result<FurDB, Box<dyn Error>> {
+        Self::ensure_db_files(&dir)?;
+
+        let db_info = if db_info.is_some() {
+            db_info.unwrap()
+        } else {
+            Self::load_info(&dir)?
+        };
+
+        Ok(FurDB { dir, db_info })
     }
 
-    pub fn get_info(&self) -> std::io::Result<FurDBInfo> {
-        let db_info_file_path = Self::get_info_file_path(&self.dir);
-        let db_info_contents_raw = std::fs::read_to_string(&db_info_file_path)?;
-        let db_info_contents = serde_json::from_str(&db_info_contents_raw)?;
-        let db_info = serde_json::from_value(db_info_contents)?;
-
-        Ok(db_info)
+    pub fn get_info(&self) -> Result<&FurDBInfo, Box<dyn Error>> {
+        Ok(&self.db_info)
     }
 }
--- src/database/utils.rs
+++ src/database/utils.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{error::Error, path::PathBuf};
 
 use crate::{FurDB, FurDBInfo};
 
@@ -10,29 +10,20 @@ impl FurDB {
         db_info_file_path
     }
 
-    pub(super) fn ensure_db_files(
-        dir: &PathBuf,
-        db_info: Option<FurDBInfo>,
-    ) -> std::io::Result<()> {
+    pub(super) fn ensure_db_files(dir: &PathBuf) -> std::io::Result<()> {
         if !dir.exists() {
             std::fs::create_dir(&dir)?;
         }
 
-        let db_info_file_path = Self::get_info_file_path(&dir);
-
-        if !db_info_file_path.exists() {
-            let db_name = dir
-                .file_name()
-                .unwrap_or(std::ffi::OsStr::new(""))
-                .to_str()
-                .unwrap_or("");
-
-            let db_info = db_info.unwrap_or(FurDBInfo::new(db_name));
-            let db_info_contents_raw = serde_json::to_string(&db_info)?;
-
-            std::fs::write(db_info_file_path, db_info_contents_raw)?;
-        }
-
         Ok(())
+    }
+
+    pub(super) fn load_info(dir: &PathBuf) -> Result<FurDBInfo, Box<dyn Error>> {
+        let db_info_file_path = Self::get_info_file_path(&dir);
+        let db_info_contents_raw = std::fs::read_to_string(&db_info_file_path)?;
+        let db_info_contents = serde_json::from_str(&db_info_contents_raw)?;
+        let db_info = serde_json::from_value(db_info_contents)?;
+
+        Ok(db_info)
     }
 }
