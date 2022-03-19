488947c538131ee1cc80ad876e071d8aa0804908
madhavan-raja <madhavanraja99@gmail.com>
2022-03-19 19:55:32 +0530

    Made row bin conversion name more descriptive

--- src/table/operations/add.rs
+++ src/table/operations/add.rs
@@ -7,25 +7,24 @@ impl FurTable {
         let mut rows_bin = BitVec::<u8, Msb0>::new();
 
         for row in rows {
-            let mut row_bin = self.add_row(row)?;
+            let mut row_bin = self.convert_row_to_bin(row)?;
             rows_bin.append(&mut row_bin);
         }
 
         let bytes: Vec<u8> = rows_bin.into();
         self.write_data(&bytes)?;
 
-        self.data_file_size = Self::get_data_file_size(&self.dir)?;
-
         Ok(())
     }
 
     pub(super) fn write_data(&mut self, bytes: &Vec<u8>) -> Result<(), Box<dyn Error>> {
         self.data_file.write(&bytes)?;
+        self.data_file_size = Self::get_data_file_size(&self.dir)?;
 
         Ok(())
     }
 
-    pub(super) fn add_row(
+    pub(super) fn convert_row_to_bin(
         &mut self,
         row: &HashMap<&str, &str>,
     ) -> Result<BitVec<u8, Msb0>, Box<dyn Error>> {
@@ -38,7 +37,6 @@ impl FurTable {
             let data = row.get(column_id).unwrap_or(&&"");
 
             let data_type = column.get_data_type();
-
             let mut column_bin = data_type.encode(
                 data,
                 column.get_size(),
