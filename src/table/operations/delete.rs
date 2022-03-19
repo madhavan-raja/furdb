23439227db258c8bb79cd5602ac7abaac674d67a
madhavan-raja <madhavanraja99@gmail.com>
2022-03-19 19:57:10 +0530

    Made row deletion operation more descriptive

--- src/table/operations/delete.rs
+++ src/table/operations/delete.rs
@@ -2,7 +2,7 @@ use crate::FurTable;
 use std::error::Error;
 
 impl FurTable {
-    pub fn delete_all_data(&self) -> Result<(), Box<dyn Error>> {
+    pub fn delete_all_rows(&self) -> Result<(), Box<dyn Error>> {
         let data_file_path = Self::get_data_file_path(&self.dir);
         std::fs::write(data_file_path, "")?;
 
