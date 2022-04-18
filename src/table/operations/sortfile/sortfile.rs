f8f22f717e95d936a597649fd7ed8c3320bf4b6b
madhavan-raja <madhavanraja99@gmail.com>
2022-04-18 20:20:20 +0530

    Changed sortfile ID variable name

--- src/table/operations/sortfile/sortfile.rs
+++ src/table/operations/sortfile/sortfile.rs
@@ -2,21 +2,18 @@ use std::error::Error;
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Sortfile {
-    column_id: String,
+    id: String,
     sortlist: Vec<u64>,
 }
 
 impl Sortfile {
-    pub fn new(column_id: &str, sortlist: &[u64]) -> Result<Self, Box<dyn Error>> {
-        let column_id = String::from(column_id);
+    pub fn new(id: &str, sortlist: &[u64]) -> Result<Self, Box<dyn Error>> {
+        let id = String::from(id);
         let sortlist = Vec::from(sortlist);
-        Ok(Sortfile {
-            column_id,
-            sortlist,
-        })
+        Ok(Sortfile { id, sortlist })
     }
 
     pub fn get_column_id(&self) -> String {
-        self.column_id.clone()
+        self.id.clone()
     }
 }
